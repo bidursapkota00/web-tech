25ff3e7b567e263b16828930b9a626bf06c07ba2
bidursapkota00 <bidursapkota00@gmail.com>
2024-06-13 13:34:12 +0545

    js started

---
+++ js/5operator.js
@@ -0,0 +1,16 @@
+// comparision operator
+let a = 1;
+let b = "1";
+
+const isEqual = a !== b; //  ===  &  ==
+console.log(isEqual);
+
+// logical operators
+const isTrue = true && false; // ||
+console.log(isTrue);
+console.log(!isTrue);
+
+// typeof operator
+const type = typeof a;
+console.log(type);
+console.log(typeof type);
